85d6d0e6fedbfca7b0f77779db982e0de96ff039
bu23 <70064197+bu23@users.noreply.github.com>
2020-09-19 13:06:57 +0900

    各モデルのテスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameの値が存在すれば登録できる事' do
+      expect(@room).to be_valid
+    end
+
+    it 'nameの値が空では登録できない事' do
+      @room.name = nil
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
